7c832130d26301abefc6019de2f602088fb60af3
mauroayala <mauro.julian.ayala@gmail.com>
2021-11-14 20:30:46 -0300

    Update evnvio2.php

--- helpers/Email/evnvio2.php
+++ helpers/Email/evnvio2.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'elece.com2@gmail.com';                     //SMTP username
-    $mail->Password   = 'seguridad16';                               //SMTP password
+    $mail->Username   = 'systemenergym@gmail.com';                     //SMTP username
+    $mail->Password   = 'okjrszxdvlfbaodu';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
